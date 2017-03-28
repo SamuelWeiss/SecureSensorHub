a1d01963c2e88ad9e629030c73b36978e1218096
samweiss250@gmail.com <jumbogiant@localhost.localdomain>
2017-03-28 15:44:29 +0000

    Move snap code into Github repo

---
+++ SSH/parts/make-project/build/server.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/hub', function(req, res){
+    res.sendFile(__dirname + '/hub.html');
+});
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/login.html');
+});
+
+app.get('/chat', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+
+io.on('connection', function(socket){
+  socket.broadcast.emit('Hello snapcrafter');
+  socket.on('chat message', function(user, msg){
+    io.emit('chat message', user, msg);
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
---
+++ SSH/parts/server/src/main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python2
+
+
+import sys
+
+if __name__ == '__main__':
+    for i in range(10):
+        print i*i
+
+    print "Python is working!"
